78301ef74b03b68d39fe4f6824b036c639ff3676
Maleklabbaci <Marcolabbaci@gmail.com>
2026-04-25 11:10:41 +0100

    Update FAQ.tsx

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { ChevronDown } from 'lucide-react';
-import { useTranslation } from 'react-i18next'; // Added translation
+import { useTranslation } from 'react-i18next';
 
 export default function FAQ() {
   const { t } = useTranslation();
